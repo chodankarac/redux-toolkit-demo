6142fb1be0bee86ae2a41042e00282410d8693ce
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-11-14 17:51:39 +0530

    configuring store;implements issue #3

--- app/store.js
+++ app/store.js
@@ -2,6 +2,7 @@ const { configureStore } = require("@reduxjs/toolkit");
 const cakeReducer = require("../features/cake/cakeSlice");
 
 const store = configureStore({
+  //instead of createstore,configurestore method used in rtk
   reducer: {
     cake: cakeReducer,
   },
